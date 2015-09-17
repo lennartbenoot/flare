f393c8eb99232362bfe6b9f24bdd2d4063b4843c
lbenoot137 <lennart.benoot@mincko.com>
2015-09-17 15:30:31 +0200

    Code clean-up

--- src/org/flare/server/FlareServer.java
+++ src/org/flare/server/FlareServer.java
@@ -6,29 +6,30 @@ import java.net.Socket;
 
 public class FlareServer {
 
+	/**
+	 * Flare Server main program
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 
 		ServerSocket serverSocket;
 		Socket socket = null;
 		try {
-			serverSocket = new ServerSocket( 9999);
-			
-			while ( true) {
+			serverSocket = new ServerSocket(9999);
+
+			while (true) {
 				socket = serverSocket.accept();
-				
-				FlareThread ft= new FlareThread();
-				ft.setConnection( socket);
+
+				FlareThread ft = new FlareThread();
+				ft.setConnection(socket);
 				ft.run();
-				
-				
+
 			}
-			
-			
+
 		} catch (IOException e) {
 			System.out.println(e);
 		}
-
-		
 
 	}
 
