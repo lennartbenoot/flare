aa58afff1e53a1e950168dbd6ee2d47c4685e445
lbenoot137 <lennart.benoot@mincko.com>
2015-10-18 11:45:33 +0200

    Updated random generation mechanism to implement new random number generation algorythm

--- src/org/flare/map/Map.java
+++ src/org/flare/map/Map.java
@@ -3,13 +3,31 @@ package org.flare.map;
 public class Map {
 	
 	
+	public static int TILE_DESERT = 1;
+	public static int TILE_DESERT_GRASS = 2; 
+	public static int TILE_DESERT_CAVE = 3; 
+	public static int TILE_DESERT_PLANT_WITH_BERRIES = 4; 
+	
+	private static MWCRandom rand = new MWCRandom( 31415926);
+	
 	public static int getTile( long x, long y) {
 		
-		long randomnumber = ( ( Math.abs(x*y)) * 214013 + 2531011) % 32768;
-		long tile = ((randomnumber) % 100);
+		// this generates a number between 0 and 32768
 		
-		return (int)tile;
+		long randomNumber = rand.nextLong( 10000);
+		
+		if ( isBetween( randomNumber, 	0		, 	500		)) return TILE_DESERT_GRASS;
+		if ( isBetween( randomNumber, 	500 	, 	502		)) return TILE_DESERT_CAVE;	
+		if ( isBetween( randomNumber, 	502 	, 	512		)) return TILE_DESERT_PLANT_WITH_BERRIES;
+	
+		return TILE_DESERT;
 	}
 	
+	
+	public static boolean isBetween( long number, long low, long high)
+	{
+		
+		return ( (number > low) && ( number  < high));
+	}
 
 }
